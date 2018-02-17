fe028dbdc8ea466cf886aecb47c2587cf7460e87
Fabrizio Bertone <fab.bertone@gmail.com>
2018-02-17 18:36:32 +0100

    initial version

---
+++ 32100_dissector.lua
@@ -0,0 +1,60 @@
+-- Wireshark dissector for IoT protocol running on UDP port 32100
+-- Author: Fabrizio Bertone <fab.bertone@gmail.com>
+-- Version: 0.0.1 (2018-02-17_1)
+-- https://github.com/fbertone/32100-dissector
+
+-- known message types
+messages = {
+  [0xf100] = "STUN request",
+  [0xf101] = "STUN response",
+  [0xf120] = "UID Lookup Request 1",
+  [0xf121] = "UID Lookup ACK",
+  [0xf140] = "UID Lookup Response 1",
+  [0xf167] = "UID Lookup Request 2",
+  [0xf169] = "UID Lookup Response 2 (TURN servers)",
+}
+
+-- declare our protocol
+IoT32100_proto = Proto("32100","32100 IoT Protocol")
+
+-- declare fields
+local F_fullnumtype = ProtoField.new("Full Numeric Type", "32100.fulltype", ftypes.UINT16, nil, base.DEC_HEX, nil, "Full Message Type (DEC_HEX)")
+local F_numtype = ProtoField.new("Numeric Type", "32100.type", ftypes.UINT8, nil, base.DEC_HEX, nil, "Short Message Type (DEC_HEX)")
+local F_strtype = ProtoField.new("Description", "32100.description", ftypes.STRING, nil, nil, nil, "Message Type Description (STRING)")
+local F_known = ProtoField.new("Known type", "32100.known", ftypes.BOOLEAN, nil, nil, nil, "Message type is known (BOOLEAN)")
+local F_paylen = ProtoField.new("Payload length", "32100.paylen", ftypes.UINT16, nil, base.DEC, nil, "Payload Length (DEC)")
+
+-- add the fields to the protocol
+-- (to confirm this worked, check that these fields appeared in the "Filter Expression" dialog)
+IoT32100_proto.fields = {F_fullnumtype, F_numtype, F_strtype, F_known, F_paylen}
+
+-- create a function to dissect it
+function IoT32100_proto.dissector(buffer,pinfo,tree)
+    pinfo.cols.protocol = "32100"
+    local full_type = buffer(0,2):uint()
+    local num_type = buffer(1,1):uint()
+    local known = false
+    local str_type = "Unknown - " .. string.format("0x%x", full_type)
+
+    if (messages[full_type]) then
+      known = true
+      str_type = messages[full_type]
+    end
+
+    local payload_len = buffer(2,2):uint()
+    local payload = buffer(4,payload_len)
+
+    local subtree = tree:add(IoT32100_proto,buffer(),"32100 Protocol")
+
+    subtree:add(F_fullnumtype, buffer(0,2), full_type)
+    subtree:add(F_numtype, buffer(1,1), num_type)
+    subtree:add(F_strtype, buffer(0,2), str_type)
+    subtree:add(F_known, buffer(0,2), known)
+    subtree:add(F_paylen, buffer(2,2), payload_len)
+
+end
+
+-- load the udp.port table
+udp_table = DissectorTable.get("udp.port")
+-- register our protocol to handle udp port 32100
+udp_table:add(32100,IoT32100_proto)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # 32100-dissector
-Wireshark dissector useful to analyze 32100 UDP protocol
+Wireshark dissector useful to analyze [32100 UDP protocol](https://github.com/fbertone/lib32100/wiki)
+
+## Install
+Copy 32100_dissector.lua in Wireshark's plugin directory (try subdirectories like wireshark or <version> if it doesn't work on plugins root)
+
+Alternatively run run Wireshark from command line like this:
+
+```bash
+wireshark -X lua_script:32100_dissector.lua
+```
+
+## Usage
+The dissector should automatically handle packets sent to or from UDP port 32100
+
+You can use filters like
+* `32100` to display only protocol's packet or
+* `32100.known == false` to check messages still not known
+* `32100.type == 0x20` to only show specific type of messages
+
+## Contributing
+Contributes of any kind are welcome.
+
+Please report unknown types in order to increase the protocol's coverage
